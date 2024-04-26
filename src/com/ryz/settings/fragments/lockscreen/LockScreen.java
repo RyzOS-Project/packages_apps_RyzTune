9e9ffde7da112dda9888b14df6503600abe5bca2
Stealth1226 <rishawn@projectlighthouse.tech>
2024-04-26 13:15:05 +0900

    RyzTune: Allow disabling ripple effect on unlock [2/2]
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- src/com/ryz/settings/fragments/lockscreen/LockScreen.java
+++ src/com/ryz/settings/fragments/lockscreen/LockScreen.java
@@ -9,6 +9,7 @@ package com.ryz.settings.fragments.lockscreen;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
@@ -30,6 +31,11 @@ public class LockScreen extends SettingsPreferenceFragment implements
 
     private static final String TAG = "LockScreen";
 
+    private static final String KEY_FINGERPRINT_CATEGORY = "lock_screen_fingerprint_category";
+    private static final String KEY_RIPPLE_EFFECT = "enable_ripple_effect";
+
+    private PreferenceCategory mFingerprintCategory;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +45,15 @@ public class LockScreen extends SettingsPreferenceFragment implements
         final ContentResolver resolver = context.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources resources = context.getResources();
+
+        mFingerprintCategory = (PreferenceCategory) findPreference(KEY_FINGERPRINT_CATEGORY);
+
+        FingerprintManager fingerprintManager = (FingerprintManager)
+                getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+
+        if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+            prefScreen.removePreference(mFingerprintCategory);
+        }
     }
 
     @Override
@@ -60,6 +75,13 @@ public class LockScreen extends SettingsPreferenceFragment implements
             public List<String> getNonIndexableKeys(Context context) {
                 List<String> keys = super.getNonIndexableKeys(context);
                 final Resources resources = context.getResources();
+
+                FingerprintManager fingerprintManager = (FingerprintManager)
+                    context.getSystemService(Context.FINGERPRINT_SERVICE);
+
+                if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+                    keys.add(KEY_RIPPLE_EFFECT);
+                }
                 return keys;
             }
         };
