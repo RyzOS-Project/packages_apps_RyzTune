62949b444d14273b53956571f32bfa8003af81c4
Pranav Vashi <neobuddy89@gmail.com>
2024-05-24 03:11:00 +0900

    RyzTune: Add separate tunables for navbar layout customization [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- src/com/ryz/settings/RyzSettings.java
+++ src/com/ryz/settings/RyzSettings.java
@@ -6,20 +6,50 @@
 
 package com.ryz.settings;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.ryz.settings.utils.DeviceUtils;
+
 @SearchIndexable
 public class RyzSettings extends DashboardFragment {
 
     private static final String TAG = "RyzSettings";
 
+    private static final String KEY_BUTTONS_PREF = "buttons";
+
+    private Preference mButtonsPref;
+
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.ryz_settings;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources resources = context.getResources();
+
+        mButtonsPref = (Preference) findPreference(KEY_BUTTONS_PREF);
+
+        if (DeviceUtils.isEdgeToEdgeEnabled(context)) {
+            prefScreen.removePreference(mButtonsPref);
+        }
     }
 
     @Override
